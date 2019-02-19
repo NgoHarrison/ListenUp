9bfbbba204cd698e5dcb18dd826e47bd23db86d4
dlonad <dhession@buffalo.edu>
2019-02-19 16:13:09 -0500

    preliminary login form stuff

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from firebase import firebase
+from Flask-Login import LoginForm
 
 app=Flask(__name__)
 firebase = firebase.FirebaseApplication('change this', None)
@@ -8,5 +9,10 @@ firebase = firebase.FirebaseApplication('change this', None)
 def home():
     return render_template("index.html")
 
+# Make this actually work
+@app.route("/loginpage", methods=["POST", "GET"])
+def login_page():
+    return render_template('Login_Page.html', form=LoginForm())
+        
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1")
