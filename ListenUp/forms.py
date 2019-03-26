7ad286056c300eb082269f71fb661c9941a0828b
Harrison Ngo <hvngo@buffalo.edu>
2019-03-26 13:39:40 -0400

    Updated project structure

---
+++ ListenUp/forms.py
@@ -0,0 +1,32 @@
+from ListenUp.models import User
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms.validators import ValidationError
+
+class SignupForm(Form):
+    username = StringField('Username', [validators.Length(min=3, max=25),validators.DataRequired()],render_kw={"placeholder": "Enter your name..."})
+    email = StringField('Email Address', [validators.Length(min=6, max=35),validators.DataRequired()],render_kw={"placeholder": "Enter your email..."})
+    password = PasswordField('Password',[
+        validators.DataRequired(),
+        validators.EqualTo('confirmpassword',message='Your passwords do not match')
+    ],render_kw={"placeholder": "Enter your password..."})
+    confirmpassword = PasswordField('Confirm password',[validators.DataRequired()],render_kw={"placeholder": "Re-enter your password..."})
+
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already exists.')
+
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already exists.')
+
+class LoginForm(Form):
+    username = StringField('Username', [validators.Length(min=3, max=25)],
+                           render_kw={"placeholder": "Enter your name..."})
+    email = StringField('Email Address', [validators.Length(min=6, max=35), validators.DataRequired()],
+                        render_kw={"placeholder": "Enter your email..."})
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirmpassword', message='Your passwords do not match')
+    ], render_kw={"placeholder": "Enter your password..."})
---
+++ ListenUp/routes.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+from .forms import SignupForm
+from passlib.hash import sha256_crypt
+from .forms import SignupForm, LoginForm
+from ListenUp import app,db
+from .models import User,Arguments
+from flask_login import login_user
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Make this actually work
+@app.route("/login_page" ,methods = ['GET','POST'])
+def login_page():
+    form = LoginForm(request.form)
+    if request.method == 'POST':
+        app.logger.info('Hereeeee')
+        user = User.query.filter_by(email = form.email.data).first()
+        if user and sha256_crypt.verify(form.password.data,user.password):
+            login_user(user)
+            return redirect(url_for('discussionhome'))
+        app.logger.info('Error')
+        flash('You have entered an incorrect email/password!')
+    return render_template("Login_Page.html")
+
+@app.route("/signup_page", methods = ['GET','POST'])
+def signup_page():
+    form = SignupForm(request.form)
+    if request.method =='POST' and form.validate():
+        encPass=sha256_crypt.encrypt(str(form.password.data))
+        user = User(username=form.username.data, email = form.email.data,password=encPass)
+        db.session.add(user)
+        db.session.commit()
+        flash('You have successfully registered!', 'success')
+        return redirect(url_for('home'))
+    return render_template("Signup_Page.html", form=form)
+
+
+@app.route("/discussionhome", methods = ['GET','POST'])
+def discussionhome():
+    return render_template("discussionhome.html")
+
+@app.route("/logout.html", methods = ['GET','POST'])
+def logout():
+    flash('You have successfully logged out!')
+    return render_template("index.html")
