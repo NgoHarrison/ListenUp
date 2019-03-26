163242fe3ac3c7aaa7b78a9059661a568d395727
apena42 <apena4@buffalo.edu>
2019-03-26 16:45:25 -0400

    Added form code, route code, and model code for Profile
    Html with backend for both discussionhome.html and editprofile.html

--- forms.py
+++ forms.py
@@ -1,12 +1,9 @@
-from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
-from passlib.hash import sha256_crypt
-import sqlite3
+from ListenUp.models import User
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
-
+from wtforms.validators import ValidationError
 
 class SignupForm(Form):
-    username = StringField('Username', [validators.Length(min=3, max=25)],render_kw={"placeholder": "Enter your name..."})
+    username = StringField('Username', [validators.Length(min=3, max=25),validators.DataRequired()],render_kw={"placeholder": "Enter your name..."})
     email = StringField('Email Address', [validators.Length(min=6, max=35),validators.DataRequired()],render_kw={"placeholder": "Enter your email..."})
     password = PasswordField('Password',[
         validators.DataRequired(),
@@ -14,6 +11,15 @@ class SignupForm(Form):
     ],render_kw={"placeholder": "Enter your password..."})
     confirmpassword = PasswordField('Confirm password',[validators.DataRequired()],render_kw={"placeholder": "Re-enter your password..."})
 
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already exists.')
+
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already exists.')
 
 class LoginForm(Form):
     username = StringField('Username', [validators.Length(min=3, max=25)],
@@ -24,3 +30,12 @@ class LoginForm(Form):
         validators.DataRequired(),
         validators.EqualTo('confirmpassword', message='Your passwords do not match')
     ], render_kw={"placeholder": "Enter your password..."})
+
+#edit profile form
+class EditProfile(Form):
+    name = StringField('name', [validators.Length(min=3, max=30)],
+                           render_kw={"placeholder": "Enter your name..."})
+    bio = StringField('bio', [validators.Length(min=6, max=200), validators.DataRequired()],
+                        render_kw={"placeholder": "Enter a bio..."})
+    title = StringField('title', [validators.Length(min=6, max=35), validators.DataRequired()],
+                        render_kw={"title": "Enter your title..."})
--- models.py
+++ models.py
@@ -1,9 +1,38 @@
-from firebase_orm import models
+from flask_login import UserMixin
 
-class Argument:
-     def __init__(self, text, votes, timestamp, user):
-          pass
-          
-class User:
-       def __init(self):
-             pass
+from ListenUp import db,login_manager
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    arguments = db.relationship('Arguments', backref='author', lazy=True)
+
+    def __repr__(self):
+        return ("")
+
+#New Profile model for Profile Database, to be linked with foreign_keys
+class Profile(db.Model):
+    name = db.Column(db.String(30), unique=True, nullable=False, primary_key=True)
+    bio = db.Column(db.String(200), unique=True, nullable=False)
+    title = db.Column(db.String(20), unique=True, nullable=False)
+
+class Arguments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return ("")
+
+
+
