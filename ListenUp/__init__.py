abf73b09e1b9bb490147771c5cdf10076c9da677
Harrison Ngo <hvngo@buffalo.edu>
2019-04-11 13:13:43 -0400

    Deleted flask migrate and flask script modules

--- ListenUp/__init__.py
+++ ListenUp/__init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
 import logging
 from flask_login import LoginManager
 
